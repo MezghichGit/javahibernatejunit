52aac3181ae9b4f9a1b37c2710a97746aa645e2b
moahmed amine mezghich <git@mezghich.com>
2023-02-13 10:33:28 +0100

    attributs d'instance + constructeurs

--- src/day1/Animal.java
+++ src/day1/Animal.java
@@ -1,5 +1,22 @@
 package day1;
 
 public class Animal {
+	// 1)Attributs d'instance (propriétés ou caractéristiques)
+	String nom;
+	int age;
+	
+	// 2)Les contructeurs
+	
+	public Animal()  // le constructeur par défaut(sans paramètres)
+	{
+		System.out.println("Construction d'un Animal");
+	}
+	
+	public Animal(String nom, int age)
+	{
+		System.out.println("Construction d'un Animal avec deux paramèters");
+		this.nom = nom;
+		this.age = age;
+	}
 
 }
--- src/day1/Application.java
+++ src/day1/Application.java
@@ -3,8 +3,20 @@ package day1;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
-
+		//System.out.println("Hello Java");
+		
+		// 1) Création d'une instance
+		
+		Animal a1 = new Animal(); // appel au contructeur par défaut
+		
+		Animal a2 = new Animal(); // appel au contructeur par défaut
+		
+		System.out.println(a1.nom);  //null
+		System.out.println(a1.age);  // 0
+        
+		Animal a3 = new Animal("Rex", 3);
+		System.out.println("Nom : " + a3.nom);
+		System.out.println("Age : " + a3.age);
 	}
 
 }
