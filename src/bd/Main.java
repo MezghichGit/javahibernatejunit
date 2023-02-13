6a2b6feadca22106563fadbf317968c0b738a75e
moahmed amine mezghich <git@mezghich.com>
2023-02-13 17:47:27 +0100

    opération d'ajout dans la base de données

--- src/bd/Main.java
+++ src/bd/Main.java
@@ -2,23 +2,35 @@ package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		Connection connection = null;
 		
 		 try {
 	     
 	      Class.forName("com.mysql.jdbc.Driver");
 	            
-	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit",
-	                "root", "");
+	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
 	     
-	     System.out.println(connection);
+	     Statement stmt = connection.createStatement();
+	     
+	     String sql ="insert into etudiant (nom,email,age) values('sam','sam.mezghich@gmail.com','24')";
+	     int res = stmt.executeUpdate(sql);
+	     System.out.println(res);
+	     
+	     //System.out.println(connection);
 		 }
 		 catch(Exception exception) {
-			 System.out.println(exception.getMessage());
+			 System.out.println("Exception : "+ exception.getMessage());
+		 }
+		 
+		 finally {
+			 if(connection!=null)
+				 connection.close();
 		 }
 
 	}
