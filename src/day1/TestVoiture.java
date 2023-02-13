599e2a9cf31994c8588e95d1212a8ae7baea1721
moahmed amine mezghich <git@mezghich.com>
2023-02-13 12:37:07 +0100

    saisie de data via la classe Scanner de java.util

--- src/day1/TestVoiture.java
+++ src/day1/TestVoiture.java
@@ -1,10 +1,12 @@
 package day1;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class TestVoiture {
 
 	public static void main(String[] args) {
+		/*
 		//nbreVoiture = 0
 		Voiture.getNbreVoitures();
 		Voiture v1 = new Voiture("BMW","Z3",280,LocalDate.of(2015, 10, 1));
@@ -21,6 +23,38 @@ public class TestVoiture {
 		System.out.println(v3);
 		
 		Voiture.getNbreVoitures(); // appel à la  méthode de classe
+		
+		*/
+		
+		System.out.println("Taper les informations de votre voiture : ");
+		
+		System.out.println("****");
+		
+		
+		Scanner sc = new Scanner(System.in); // création d'un lecture du clavier
+		
+		System.out.println("Taper la marque : ");
+		String marque = sc.nextLine(); // nextLine() : méthode pour lire une chaine de caractère
+		
+		//System.out.println("Marque = "+marque);
+		
+		System.out.println("Taper le modèle : ");
+		String modele = sc.nextLine();
+		
+		System.out.println("Taper la date immatriculation sous la forme : 2010-10-1");
+		String dateImmatriculation = sc.nextLine();
+		LocalDate dateImmat = LocalDate.parse(dateImmatriculation);
+		
+		System.out.println("Taper vitesse max : ");
+		int vitesseMax = sc.nextInt();
+		/*
+		System.out.println("Marque = "+marque);
+		System.out.println("Modele = "+modele);
+		System.out.println("DateImmat = "+dateImmat);
+		System.out.println("VitesseMax = "+vitesseMax);*/
+		
+		Voiture v1 = new Voiture(marque,modele,vitesseMax,dateImmat);
+		System.out.println(v1);
 
 	}
 
