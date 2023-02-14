2dec70998b25e12695ac7ca35773641b0e6b7a64
moahmed amine mezghich <git@mezghich.com>
2023-02-14 12:32:01 +0100

    Part 1 du mini projet

--- src/day2/CrudEcole.java
+++ src/day2/CrudEcole.java
@@ -2,20 +2,42 @@ package day2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class CrudEcole {
 	
-	public static void addEcole(Statement stmt)
-	{
-		//...
-	}
 	
-	public static void displayEcoles(Statement stmt)
-	{
-		//...
-	}
+	public static int addEcole(Statement stmt) throws SQLException
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donner le nom de votre école");
+        String nom = sc.nextLine();
+        System.out.println("Donner l'adresse de votre école");
+        String adresse = sc.nextLine();
+        System.out.println("Donner le nombre total d'étudiant de l'école");
+        int totaletudiant = sc.nextInt();
+        // 1) Insert dans la table ecole
+        String sql = "insert into ecole (nom,adresse,totaletudiants) values('" + nom + "','" + adresse + "','" + totaletudiant + "')";
+        return stmt.executeUpdate(sql);
+    }
+    
+	
+	public static void displayEcoles(Statement stmt) throws SQLException
+    {
+        ResultSet rs = null;
+        String rq = "select * from ecole";
+        rs = stmt.executeQuery(rq);
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String nom = rs.getString("nom");
+            String adresse = rs.getString("adresse");
+            int totaletudiant = rs.getInt("totaletudiants");
+            System.out.println("ID : " + id + " | Nom : " + nom + " | Adresse : " + adresse + " | Totaletudiant : " + totaletudiant);
+        }
+    }
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = null;
