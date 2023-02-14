54a59d93100dfa5a2e3d5a4724cd19add7502b66
moahmed amine mezghich <git@mezghich.com>
2023-02-14 11:05:07 +0100

    méthode update + refactoring du code

--- src/bd/Main.java
+++ src/bd/Main.java
@@ -8,103 +8,153 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static void getData(Statement stmt) throws SQLException
-	{
+
+	public static void getData(Statement stmt) throws SQLException {
 		ResultSet rs = null;
 		String rq = "select * from etudiant";
-	     
-	     rs = stmt.executeQuery(rq);
-	     
-	     while(rs.next()) {
-	    	 
-	    	 int id = rs.getInt("id");
-	    	 String nom = rs.getString("nom");
-	    	 String email = rs.getString("email");
-	    	 int age= rs.getInt("age");
-	    	 
-	    	 System.out.println("ID : "+id+" | Nom : "+nom+" | Email : "+email+" | Age : "+age);
-	     }
+
+		rs = stmt.executeQuery(rq);
+
+		while (rs.next()) {
+
+			int id = rs.getInt("id");
+			String nom = rs.getString("nom");
+			String email = rs.getString("email");
+			int age = rs.getInt("age");
+
+			System.out.println("ID : " + id + " | Nom : " + nom + " | Email : " + email + " | Age : " + age);
+		}
 	}
 
-	
-	public static int insertData(Statement stmt) throws SQLException
-	{
-		 Scanner sc = new Scanner(System.in);
-	     
-	     System.out.println("Donner votre nom");
-	     String nom=sc.nextLine();
-	     
-	     System.out.println("Donner votre email");
-	     String email=sc.nextLine();
-	     
-	     System.out.println("Donner votre age");
-	     int age=sc.nextInt();
-	     
-	     //1) Insert dans la table Etudiant
-	      
-	     String sql ="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
-	     int res = stmt.executeUpdate(sql);
-	     //System.out.println(res);
-	     return res;
+	public static int insertData(Statement stmt) throws SQLException {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Donner votre nom");
+		String nom = sc.nextLine();
+
+		System.out.println("Donner votre email");
+		String email = sc.nextLine();
+
+		System.out.println("Donner votre age");
+		int age = sc.nextInt();
+
+		// 1) Insert dans la table Etudiant
+
+		String sql = "insert into etudiant (nom,email,age) values('" + nom + "','" + email + "','" + age + "')";
+		int res = stmt.executeUpdate(sql);
+		// System.out.println(res);
+		return res;
 	}
+
+	public static int deleteData(Statement stmt) throws SQLException {
+		int res =0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Donner un id");
+		int id = sc.nextInt();
+		
+		if (existe(id, stmt)) {
+
+		String sql = "delete from etudiant where id ='" + id + "'";
+		res = stmt.executeUpdate(sql);
+
 	
-	public static int deleteData(Statement stmt) throws SQLException
-	{
-		 Scanner sc = new Scanner(System.in);
-	     
-	     System.out.println("Donner un id");
-	     int id=sc.nextInt();
-	     
-	      
-	     String sql ="delete from etudiant where id ='"+id+"'";
-	     int res = stmt.executeUpdate(sql);
-	     
-	     if(res == 1)
-	     {
-	    	 System.out.println("Suppression avec sucès");
-	     }
-	     else
-	     {
-	    	 System.out.println("Etudiant introuvable");
-	     }
-	     return res;
+			System.out.println("Suppression avec sucès");
+		} else {
+			System.out.println("Etudiant introuvable");
+		}
+		return res;
 	}
-	
-	public static void main(String[] args)  throws Exception{
+
+	// public void somme(int a, int b) // méthode d'insatnce
+
+	// public static int somme(int a, int b) // méthode de classe
+	/*
+	 * public static void somme(int a, int b) { System.out.println("La somme = "+
+	 * (a+b)); //return (a+b); }
+	 */
+
+	public static int updateData(Statement stmt) throws SQLException {
+		int res = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Donner un id");
+		int id = sc.nextInt();
+		sc.nextLine();// recupérer le click sur lt btn entré
+
+		if (existe(id, stmt)) {
+			System.out.println("Donner un nom");
+			String nom = sc.nextLine();
+
+			String sql = "update etudiant set nom='" + nom + "' where id ='" + id + "'";
+			res = stmt.executeUpdate(sql);
+			System.out.println("Mise à jour avec sucès");
+
+		}
+
+		else {
+			System.out.println("Etudiant introuvable");
+		}
+
+		return res;
+
+	}
+
+	public static boolean existe(int id, Statement stmt) throws SQLException {
+		ResultSet rs = null;
+
+		String rq = "select * from etudiant where id = '" + id + "'";
+
+		rs = stmt.executeQuery(rq);
+
+		while (rs.next()) {
+			if (rs.getInt("id") == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// Main m = new Main(); // création d'une instance
+		// m.somme(10,2);
+		// Main.somme(10,20);
+		// int res = somme(10,20);
+
 		Connection connection = null;
 		Statement stmt = null;
-		//ResultSet rs = null;
-		
-		 try {
-	     
-	      Class.forName("com.mysql.jdbc.Driver");
-	            
-	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
-	     //System.out.println(connection);
-	      stmt = connection.createStatement();
-	     
-		      //1)Lecture de data depuis le clavier
-		      //insertData(stmt);
-		      //2)Affichage de toutes les data de la table étudiant
-		      getData(stmt);
-		      //3)Suppression d'un étduiant
-		      deleteData(stmt);
-		      getData(stmt);
-	     
-	     
-	     //System.out.println(connection);
-		 }
-		 catch(Exception exception) {
-			 System.out.println("Exception : "+ exception.getMessage());
-		 }
-		 
-		 finally {
-			 if(connection!=null)
-				 connection.close();
-		 }
-		 
-		 System.out.println("Fin du programme...");
+		// ResultSet rs = null;
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit", "root", "");
+			// System.out.println(connection);
+			stmt = connection.createStatement();
+
+			// 1)Lecture de data depuis le clavier
+			// insertData(stmt);
+			// 2)Affichage de toutes les data de la table étudiant
+			getData(stmt);
+			// 3)Suppression d'un étduiant
+			 deleteData(stmt);
+			// 4)Update
+			//updateData(stmt);
+			getData(stmt);
+
+			// System.out.println(connection);
+		} catch (Exception exception) {
+			System.out.println("Exception : " + exception.getMessage());
+		}
+
+		finally {
+			if (connection != null)
+				connection.close();
+		}
+
+		System.out.println("Fin du programme...");
 
 	}
 
