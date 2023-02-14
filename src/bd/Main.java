8af02b3a7d8caec8e8d858609f390008b74560cf
moahmed amine mezghich <git@mezghich.com>
2023-02-14 09:26:04 +0100

    l'opération select

--- src/bd/Main.java
+++ src/bd/Main.java
@@ -2,13 +2,14 @@ package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args)  throws Exception{
 		Connection connection = null;
 		
 		 try {
@@ -16,9 +17,9 @@ public class Main {
 	      Class.forName("com.mysql.jdbc.Driver");
 	            
 	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
-	     
+	     //System.out.println(connection);
 	     Statement stmt = connection.createStatement();
-	     
+	     /*
 	     Scanner sc = new Scanner(System.in);
 	     
 	     System.out.println("Donner votre nom");
@@ -33,6 +34,24 @@ public class Main {
 	     String sql ="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
 	     int res = stmt.executeUpdate(sql);
 	     System.out.println(res);
+	     */
+	     
+	     //2)Affichage de toutes les data de la table étudiant
+	     
+	     String rq = "select * from etudiant";
+	     
+	     ResultSet rs = stmt.executeQuery(rq);
+	     
+	     while(rs.next()) {
+	    	 
+	    	 int id = rs.getInt("id");
+	    	 String nom = rs.getString("nom");
+	    	 String email = rs.getString("email");
+	    	 int age= rs.getInt("age");
+	    	 
+	    	 System.out.println("ID : "+id+" | Nom : "+nom+" | Email : "+email+" | Age : "+age);
+	     }
+	     
 	     
 	     //System.out.println(connection);
 		 }
@@ -44,6 +63,8 @@ public class Main {
 			 if(connection!=null)
 				 connection.close();
 		 }
+		 
+		 System.out.println("Fin du programme...");
 
 	}
 
