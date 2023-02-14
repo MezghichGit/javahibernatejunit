eb3fa6c3553e7ac310a0fd8718be49a4edb64362
moahmed amine mezghich <git@mezghich.com>
2023-02-14 09:38:29 +0100

    refactoring du code

--- src/bd/Main.java
+++ src/bd/Main.java
@@ -8,39 +8,13 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args)  throws Exception{
-		Connection connection = null;
-		
-		 try {
+	
+	public static void getData(Statement stmt) throws SQLException
+	{
+		ResultSet rs = null;
+		String rq = "select * from etudiant";
 	     
-	      Class.forName("com.mysql.jdbc.Driver");
-	            
-	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
-	     //System.out.println(connection);
-	     Statement stmt = connection.createStatement();
-	     /*
-	     Scanner sc = new Scanner(System.in);
-	     
-	     System.out.println("Donner votre nom");
-	     String nom=sc.nextLine();
-	     
-	     System.out.println("Donner votre email");
-	     String email=sc.nextLine();
-	     
-	     System.out.println("Donner votre age");
-	     int age=sc.nextInt();
-	     
-	     String sql ="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
-	     int res = stmt.executeUpdate(sql);
-	     System.out.println(res);
-	     */
-	     
-	     //2)Affichage de toutes les data de la table étudiant
-	     
-	     String rq = "select * from etudiant";
-	     
-	     ResultSet rs = stmt.executeQuery(rq);
+	     rs = stmt.executeQuery(rq);
 	     
 	     while(rs.next()) {
 	    	 
@@ -51,6 +25,46 @@ public class Main {
 	    	 
 	    	 System.out.println("ID : "+id+" | Nom : "+nom+" | Email : "+email+" | Age : "+age);
 	     }
+	}
+
+	
+	public static int insertData(Statement stmt) throws SQLException
+	{
+		 Scanner sc = new Scanner(System.in);
+	     
+	     System.out.println("Donner votre nom");
+	     String nom=sc.nextLine();
+	     
+	     System.out.println("Donner votre email");
+	     String email=sc.nextLine();
+	     
+	     System.out.println("Donner votre age");
+	     int age=sc.nextInt();
+	     
+	     //1) Insert dans la table Etudiant
+	      
+	     String sql ="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
+	     int res = stmt.executeUpdate(sql);
+	     //System.out.println(res);
+	     return res;
+	}
+	public static void main(String[] args)  throws Exception{
+		Connection connection = null;
+		Statement stmt = null;
+		//ResultSet rs = null;
+		
+		 try {
+	     
+	      Class.forName("com.mysql.jdbc.Driver");
+	            
+	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
+	     //System.out.println(connection);
+	      stmt = connection.createStatement();
+	     
+		     //1)Lecture de data depuis le clavier
+		      insertData(stmt);
+		     //2)Affichage de toutes les data de la table étudiant
+		     getData(stmt);
 	     
 	     
 	     //System.out.println(connection);
